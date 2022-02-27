60c2096fc7513ee5b46e821db16f2e852b1df2e5
ijayzo <97761169+ijayzo@users.noreply.github.com>
2022-02-26 23:15:19 -0500

    Add files via upload

--- Email/HELP.md
+++ Email/HELP.md
@@ -1,7 +1,6 @@
 # Getting Started
 
 ### Reference Documentation
-
 For further reference, please consider the following sections:
 
 * [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
@@ -12,7 +11,6 @@ For further reference, please consider the following sections:
 * [Spring Security](https://docs.spring.io/spring-boot/docs/2.6.3/reference/htmlsingle/#boot-features-security)
 
 ### Guides
-
 The following guides illustrate how to use some features concretely:
 
 * [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
---
+++ Email/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.example.demo.Services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/*
+*.class is using the main class as a root class... this is apllication context
+*passing in basic class that is going to startup the application context
+*application context = eagerly instantiates beans... makes beans ... opens up new ability to support new modules for bean
+*bean factory(?) = making beans
+*spring bean = java class/object that is being managed by spring
+*lazy initialization vs eager initialization.
+*impacted at runtime. initializes on demand.
+*IOC give control to other object
+*spring boot gives control to app.context to create and inject beans where rerquesteded
+*spring boot = injection container. creates application context. what does it want
+*	is spring trying to inject and create for me
+*
+* Spring Bena Life Cycle : Constructor and Setter Methods
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+	private MessageService service;
+	@Autowired
+	public void setService(MessageService service) {
+		this.service = service;
+	}
+
+	public static void main(String[] args){
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(){
+		return args -> {
+			System.out.println(service.getMessage());
+		};
+	}
+}
